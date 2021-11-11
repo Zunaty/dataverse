97ed567b1f196c459b874cbe54a82ee31cc4f9a6
Sean <sdillon215@gmail.com>
2021-11-10 20:00:32 -0800

    auth

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,9 +3,26 @@ const { gql } = require('apollo-server-express');
 
 // create our typeDefs
 const typeDefs = gql`
-  type Query {
-    helloWorld: String
-  }
+
+type User {
+    _id: ID
+    username: String
+    email: String
+}
+
+type Auth {
+    token: ID!
+    user: User
+}
+
+type Query {
+    users: [User]
+}
+
+type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    
+}
 `;
 
 // export the typeDefs
--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 // import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
+const { authMiddleware } = require('./utils/auth');
+
 
 // import our typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas');
@@ -13,8 +15,8 @@ const startServer = async () => {
     // create a new Apollo server and pass in our schema data
     const server = new ApolloServer({
         typeDefs,
-        resolvers
-        // context: authMiddleware
+        resolvers,
+        context: authMiddleware
     });
 
     // Start the Apollo server
