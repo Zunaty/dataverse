d334818a06d67804618443a1ceef6048076db788
Heff <shmikester@gmail.com>
2021-11-21 20:35:15 -0700

    fixed mobile home

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,13 +1,13 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid';
-import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import mainPageImage from '../../assets/images/DataVerseMainPage2.png'
 
 const theme = createTheme();
 
-function Home() {
+function Home()
+{
   // const navigate = useNavigate();
 
   // const goToLogin = async event => {
@@ -30,20 +30,16 @@ function Home() {
         alignItems="center"
         justifyContent="center"
       >
-        <ThemeProvider theme={theme}>
-          <Container maxWidth="xs">
-          <Container sx={{ ml: 18.5, mb: 5}}>
-              <Button href="/login" variant="contained">
-                {"Login"}
-              </Button>
-            </Container>
-            <Container sx={{ ml: 5 }}>
-              <Button href="/signup" variant="contained">
-                {"Don't have an account? Sign Up"}
-              </Button>
-            </Container>
-          </Container>
-        </ThemeProvider>
+        <Button href="/login" variant="contained" sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          p: 1,
+          m: 1}}>
+          {"Login"}
+        </Button>
+        <Button href="/signup" variant="contained">
+          {"Don't have an account? Sign Up"}
+        </Button>
       </Grid>
     </div >
   );
